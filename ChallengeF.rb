38fa3feab42af0ce3fe4844cf3c6f52c99cee189
liyahedwards-creator <liyah_edwards@student.davincischools.org>
2026-03-03 11:58:37 -0800

    Create ChallengeF.rb

---
+++ ChallengeF.rb
@@ -0,0 +1,15 @@
+num = 21
+
+87.times do
+  play(num)
+  sleep 0.1
+  num = num + 1
+end
+
+87.times do
+  play(num)
+  sleep 0.25
+  num = num - 1
+end
+sample :drum_splash_hard
+print("HAPPY NEW YEAR")
